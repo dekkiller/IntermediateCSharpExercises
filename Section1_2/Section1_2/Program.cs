ca19b83e6973002534011707b06b7239700be13e
dekkiller <dennissimon1993@gmail.com>
2017-10-02 20:25:36 -0700

    Stopwatch

---
+++ Section1_2/Section1_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Section1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch.Start();
+            Thread.Sleep(5000);
+            Stopwatch.End();
+            Console.WriteLine(Stopwatch.Duration);
+        }
+    }
+}
---
+++ Section1_2/Section1_2/Stopwatch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Section1_2
+{
+    static class Stopwatch
+    {
+        public static TimeSpan Duration { get; private set; }
+        private static DateTime? _startTime = null;
+        private static DateTime? StartTime
+        {
+            get
+            {
+                Console.WriteLine("In getter");
+                return _startTime;
+            }
+            set
+            {
+                Console.WriteLine("In setter");
+                if (_startTime != null)
+                {
+                    throw new InvalidOperationException("Can't call stopwatch twice without stopping!");
+                }
+                else
+                {
+                    _startTime = value;
+                }
+            }
+        }
+        //private static DateTime EndTime { get; set; }
+
+
+        public static void Start()
+        {
+            Console.WriteLine("Started Stopwatch.");
+            StartTime = DateTime.Now;
+        }
+
+        public static void End()
+        {
+            Console.WriteLine("Ended Stopwatch.");
+            Duration = DateTime.Now.Subtract(StartTime.Value);
+
+        }
+
+
+    }
+}
